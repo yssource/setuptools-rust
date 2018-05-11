2fa19fd5255ce2ff0f06b69b19e3ee747aa793e0
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-05-11 15:28:45 +0200

    Add example `build.rs` script to one of `example_tomlgen` extensions

--- example_tomlgen/hello/english/lib.rs
+++ example_tomlgen/hello/english/lib.rs
@@ -5,6 +5,10 @@ extern crate pyo3;
 use pyo3::prelude::*;
 use pyo3::py::modinit;
 
+pub mod built_info {
+   include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
 /// Module documentation string
 #[modinit("english")]
 fn init(py: Python, m: &PyModule) -> PyResult<()> {
@@ -12,6 +16,7 @@ fn init(py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "hello")]
     fn hello(_py: Python) -> PyResult<()> {
         println!("Hello, world!");
+        println!("(by the way, I was built on {})", built_info::RUSTC_VERSION);
         Ok(())
     }
 
